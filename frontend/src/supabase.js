762650c01a329f487364348eca84a73ede9da47e
cynicalmindset <yashraj.02594656@gmail.com~>
2026-05-10 19:56:47 +0530

    dot

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
+
 const supabaseURL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 const supabaseAnonkey = import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
 export const supabase = createClient(supabaseURL, supabaseAnonkey);
