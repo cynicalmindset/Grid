ff6032ff4a17eaa43421dda550d5023827fee5c7
cynicalmindset <yashraj.02594656@gmail.com~>
2026-05-20 10:14:06 +0530

    ci pipeline

--- frontend/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseURL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseAnonkey = import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-export const supabase = createClient(supabaseURL, supabaseAnonkey);
